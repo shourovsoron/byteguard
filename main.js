9ff0f9adc0ae32e9c1f36c86b62b25761ee6285c
Shourov Soron <shourovhossainsoron@gmail.com>
2024-09-17 13:19:38 +0600

    update language

--- main.js
+++ main.js
@@ -2,6 +2,10 @@ let mobileMenuBtn = document.querySelector(".mobile-menu-btn");
 let mobileBtnImg = document.querySelector(".mobile-menu-btn img");
 
 const primaryNav=document.querySelector(".primary-navigation");
+const SelectLng=document.querySelector(".select_lng");
+const SelectLngMob=document.querySelector(".select_lng_mob");
+const lngflag=document.querySelector(".country-flag");
+const lngflagMob=document.querySelector(".country-flag-mob");
 
 
 mobileMenuBtn.addEventListener('click', ()=>{
@@ -13,4 +17,23 @@ mobileMenuBtn.addEventListener('click', ()=>{
       
         mobileBtnImg.src="./images/icon-hamburger.svg"
     }
+});
+
+
+SelectLng.addEventListener('change', (e)=>{
+   let lng = e.target.value;
+
+   lngflag.src = `./images/${lng}.png`
+
+
 })
+
+
+
+SelectLngMob.addEventListener('change', (e)=>{
+    let lng = e.target.value;
+ 
+    lngflagMob.src = `./images/${lng}.png`
+ 
+ 
+ })
